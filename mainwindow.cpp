6dd76a16f58513d5135a107396c4f6adee33ccd0
Josue Ttito <josue.ttito@ucsp.edu.pe>
2018-11-14 23:54:39 -0500

    add bmp reader

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,78 +1,87 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QtWidgets>
+#include <QMessageBox>
 #include <stdio.h>
 
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
+#include <iterator>
 
-unsigned char* readBMP(char* filename)
+struct Pixel {
+    int r;
+    int g;
+    int b;
+
+    Pixel(int r,int g,int b){
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+    Pixel() {
+        r = g = b = 0;
+    }
+};
+
+struct FileData {
+    Pixel ** matrix;
+    int width;
+    int height;
+};
+
+
+FileData * ReadBMP(char* filename)
 {
     int i;
     FILE* f = fopen(filename, "rb");
-    unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
 
-    // extract image height and width from header
-    int width = *(int*)&info[18];
-    int height = *(int*)&info[22];
-
-    int size = 3 * width * height;
-    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
-    fclose(f);
-
-    for(i = 0; i < size; i += 3)
-    {
-            unsigned char tmp = data[i];
-            data[i] = data[i+2];
-            data[i+2] = tmp;
+    if(f == NULL) {
+        throw "Argument Exception";
     }
 
-    return data;
-}
-
-
-unsigned char* ReadBMP(char* filename)
-{
-    int i;
-    FILE* f = fopen(filename, "rb");
-
-    if(f == NULL)
-        throw "Argument Exception";
-
     unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+    int HEADER_SIZE = 54;
+    fread(info, sizeof(unsigned char), HEADER_SIZE, f); // read the 54-byte header
 
     // extract image height and width from header
+    int dataOffset = *(int*)&info[10];
     int width = *(int*)&info[18];
     int height = *(int*)&info[22];
 
-//    cout << endl;
-//    cout << "  Name: " << filename << endl;
-//    cout << " Width: " << width << endl;
-//    cout << "Height: " << height << endl;
+    qDebug("width: %d", width);
+    qDebug("height: %d", height);
+    qDebug("dataOffset: %d", dataOffset);
 
     int row_padded = (width*3 + 3) & (~3);
+    qDebug("row_padded : %d", row_padded);
     unsigned char* data = new unsigned char[row_padded];
     unsigned char tmp;
+    Pixel ** matrix;
+    matrix = new Pixel * [height];
+    fread(data, sizeof(unsigned char), dataOffset - HEADER_SIZE, f);
 
     for(int i = 0; i < height; i++)
     {
         fread(data, sizeof(unsigned char), row_padded, f);
+        matrix[i] = new Pixel[width];
         for(int j = 0; j < width*3; j += 3)
         {
             // Convert (B, G, R) to (R, G, B)
-            tmp = data[j];
-            data[j] = data[j+2];
-            data[j+2] = tmp;
-
-//            cout << "R: "<< (int)data[j] << " G: " << (int)data[j+1]<< " B: " << (int)data[j+2]<< endl;
+            matrix[i][j/3] = Pixel((int) data[j + 2], (int) data[j+1], (int) data[j]);
         }
     }
 
-    fclose(f);
-    return data;
-}
+    FileData * fd = new FileData();
+    fd->matrix = matrix;
+    fd->width = width;
+    fd->height = height;
 
+    fclose(f);
+    return fd;
+}
 
 
 
@@ -81,39 +90,40 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    int width    = 630;
-    int height = 354;
-    QImage image = QImage(width, height, QImage::Format_RGB32);
-
-    char * f = "/home/josue/Devel/jotoshop/tiger.bmp";
-    unsigned char* imageData = readBMP(f);
-    int p = 0;
-    int q = 100;
-
-    for(int i = 1881; i < 1981; i++) {
-        qDebug("%d - ", (int) imageData[i]);
-    }
-//    for(int x=0; x < q; x++) {
-//        for(int y=0; y < q; y++) {
-//            p = (x * width + y) * 3;
-////            qDebug("-> %d", p);
-//            int r = (int) imageData[p];
-//            int g = (int) imageData[p + 1];
-//            int b = (int) imageData[p + 2];
-//            qDebug("color (%d, %d) %d - rgb(%d, %d, %d)\n", x, y, p, r, g, b);
-//            image.setPixelColor(x, y, qRgb(r, g, b));
-//        }
-//    }
-
-    qDebug("P size %d\n", width * height);
-
-    QGraphicsScene * graphic = new QGraphicsScene(this);
-    graphic->addPixmap(QPixmap::fromImage(image));
-    ui->graphicsView->setScene(graphic);
-
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_applyButton_clicked()
+{
+    QString qFileName = QFileDialog::getOpenFileName(this,
+            tr("Open Image"), "",
+            tr("Image (*.bmp);;"));
+//    qDebug(qUtf8Printable(fileName));
+    QByteArray ba = qFileName.toLocal8Bit();
+    char *fileName = ba.data();
+
+    FileData * fd = ReadBMP(fileName);
+    int width = fd->width;
+    int height = fd->height;
+    QImage image = QImage(width, height, QImage::Format_RGB32);
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            Pixel pixel = fd->matrix[i][j];
+            image.setPixelColor(j, height - i - 1, qRgb(pixel.r, pixel.g, pixel.b));
+        }
+    }
+
+    QGraphicsScene * graphic = new QGraphicsScene(this);
+    graphic->addPixmap(QPixmap::fromImage(image));
+    ui->graphicsView->setScene(graphic);
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_applyButton_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
